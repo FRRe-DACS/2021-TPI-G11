9456f708c64ff49495f47df11c0adfe4b6948af6
NoSens29 <ruben7are@gmail.com>
2021-07-16 16:58:18 -0300

    pre subida final

--- backend/config/boots.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-  const PORT = app.get("port");
-
-  app.listen(PORT, () => {
-    console.log(`API REST corriendo en http://localhost:${PORT}`);
-  });
-};
--- backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-  app.get("/", (req, res) => {
-    console.log("GET /");
-
-    res.status(200).send({ message: "API EMPRESAS FUNCIONANDO." });
-  });
-};
